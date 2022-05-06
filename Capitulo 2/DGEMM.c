65835ab6e013d12250c95dcdfec400981d44b0e6
João Pedro Brandão e Silva <38329386+jpbrs@users.noreply.github.com>
2022-05-05 23:44:45 -0300

    Adicionando codigo DGEMM com parametro N

--- Capitulo 2/DGEMM.c
+++ Capitulo 2/DGEMM.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Programa do Livro */
+/////////////* Programa do Livro *//////////////////////
+
 void dgemm(int n, double* A, double* B, double* C){
     for (int i = 0; i < n; ++i){
         for (int j = 0; j < n; ++j){
@@ -12,24 +14,82 @@ void dgemm(int n, double* A, double* B, double* C){
         }
     }
 }
-
-int main(void)
+//***************************************************//
+double randfrom(double min, double max) 
 {
-    double a[] = { 1.0, 2.0,
-                    2.0, 1.0}; // 2x2
+    double range = (max - min); 
+    double div = RAND_MAX / range;
+    return min + (rand() / div);
+}
 
-    double b[] = { 1.0, 1.0,
-                     1.0, 1.0}; // 2x2
+double* vetor(int n){
+    double* temp;
+    temp = (double*) malloc(n*sizeof(double));
+    return temp;
+}
 
-    double c[] = { 0.00, 0.00,
+int main(int argc, char *argv[])
+{
+    char *argumento = argv[1];
+    int n = atoi(argumento);
+    int len = n*n;
+
+    double A[] = { 1.0, 2.0,
+                    3.0, 4.0}; // 2x2
+    double B[] = { 5.0, 6.0,
+                     7.0, 8.0}; // 2x2
+    double C[] = { 0.00, 0.00,
                    0.00, 0.00 }; // 2x2
 
     /* Compute C = AB */
+    dgemm(2, A, B, C);
+
+    printf("\n Feito A x B \n");
+    printf("\nMatriz A - Linha \n");
+    printf("[ %g, %g ]\n",  A[0], A[2]);
+    printf("[ %g, %g ]\n", A[1], A[3]);
+    printf("\nMatriz B - Coluna \n");
+    printf("[ %g, %g ]\n",  B[0], B[2]);
+    printf("[ %g, %g ]\n", B[1], B[3]);
+    printf("\n--- Matriz Resultado ---\n");
+    printf("[ %g, %g ]\n",  C[0], C[2]);
+    printf("[ %g, %g ]\n", C[1], C[3]);
+
+    double * a = vetor(len);
+    for (int i = 0; i<len; i++){
+        a[i] = randfrom(1.0, 10.0);
+    }
+    double * b = vetor(len);
+    int w = 0;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            if (i==j){
+                b[w] = 1;
+            }
+            else{
+                b[w] = 0;
+            }
+            w++;
+        }
+    }
+    // for (int i = 0; i<len; i++){
+    //     b[i] = randfrom(1.0, 10.0);
+    // }
+    double * c = vetor(len);  // Por que double c[len] != double c[4]
 
     dgemm(2, a, b, c);
 
-    printf("[[ %g, %g ]\n",  c[0], c[1]);
-    printf(" [ %g, %g ]]\n", c[2], c[3]);
+    printf("\n Feito a x b \n");
+    printf("\nMatriz a - Linha \n");
+    printf("[ %g, %g ]\n",  a[0], a[2]);
+    printf("[ %g, %g ]\n", a[1], a[3]);
+    printf("\nMatriz b - Coluna \n");
+    printf("[ %g, %g ]\n",  b[0], b[2]);
+    printf("[ %g, %g ]\n", b[1], b[3]);
+    printf("\n--- Matriz Resultado ---\n");
+    printf("[ %g, %g ]\n",  c[0], c[2]);
+    printf("[ %g, %g ]\n", c[1], c[3]);
+
 
     return 0;
 }
